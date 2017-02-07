3df6f573366a160cf48f4d947bfc33b7f6b72588
TDowney92 <tmed425@hotmail.com>
2017-02-07 15:37:10 -0500

    work on links

--- ex-port/routes/index.js
+++ ex-port/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about */
+router.get('/about', function(req, res, next) {
+	res.render('about');
+});
+
+/* GET contact */
+router.get('/contact', function(req, res, next) {
+	res.render('contact');
+});
+
+/* GET port */
+router.get('/port', function(req, res, next) {
+	res.render('port');
+});
+
+/* GET services */
+router.get('/services', function(req, res, next) {
+	res.render('services');
+});
+
 module.exports = router;
